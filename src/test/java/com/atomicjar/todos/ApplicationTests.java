db03d8f848740196e66891fe8dcb0e87cc3a6411
eparlato <edoardo.parlato@qmates.tech>
2026-04-25 17:36:40 +0200

    Remove unused imports

--- src/test/java/com/atomicjar/todos/ApplicationTests.java
+++ src/test/java/com/atomicjar/todos/ApplicationTests.java
@@ -10,12 +10,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.List;
 
