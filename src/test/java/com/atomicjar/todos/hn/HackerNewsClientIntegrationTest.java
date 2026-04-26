e92b461d7c184c81ffc1c496afdac19f4d2f0d78
eparlato <edoardo.parlato@qmates.tech>
2026-04-26 20:50:47 +0200

    Add to-be-completed integration test for HackerNewsClient

---
+++ src/test/java/com/atomicjar/todos/hn/HackerNewsClientIntegrationTest.java
@@ -0,0 +1,25 @@
+package com.atomicjar.todos.hn;
+
+import org.junit.jupiter.api.Test;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.wiremock.integrations.testcontainers.WireMockContainer;
+
+@Testcontainers
+class HackerNewsClientIntegrationTest {
+
+    @Container
+    static WireMockContainer wireMock = new WireMockContainer("wiremock/wiremock:3.1.0")
+        .withMappingFromResource("hackernews", "hackernews_v0-stubs.json");
+
+    @Test
+    void shouldFetchItem() {
+        HackerNewsClient client = new HackerNewsClient(wireMock.getBaseUrl());
+
+        // TODO complete
+    }
+
+    // TODO should fetch top stories id
+
+    // TODO error scenarios to be tested: 500, 404, empty list, malformed JSON
+}
